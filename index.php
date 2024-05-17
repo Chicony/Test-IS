c96ffab575fc865a24d127cf51b02d47f4b5c373
Chicony <Chicony@chicony2753@gmail.com>
2024-05-17 14:31:02 +0300

    Loading the project

---
+++ index.php
@@ -0,0 +1,464 @@
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+$APPLICATION->SetTitle("Grow your revenue with iSpring");
+?><div class="content">
+	<div class="content__partner-program partner">
+		<div class="partner__text-block">
+			<p class="partner__first-text">
+				 Partner Program
+			</p>
+			<h1 class="partner__title">Grow your revenue with iSpring</h1>
+			<p class="partner__text">
+				 Offer your customers award-winning software for eLearning authoring and training management.
+			</p>
+ <a href="#form" class="partner__button button">Become a Partner</a>
+		</div>
+		<div class="partner__photo-block photo-block">
+ <img alt="Partner Program" src="/local/templates/empty/images/top.png"> <img alt="handshake" src="/local/templates/empty/images/icons/handshake.svg" class="photo-block__handshake"> <img alt="coin ladder" src="/local/templates/empty/images/icons/coin_ladder.svg" class="photo-block__coin-ladder">
+		</div>
+	</div>
+	<div class="content__enthusiasts enthusiasts" id="enthusiasts">
+		<div class="enthusiasts__text-block">
+			<h2 class="enthusiasts__title title">Join the team of eLearning enthusiasts</h2>
+			<p class="enthusiasts__text text">
+				 Tap into the rapidly growing eLearning market with the industry leader in course authoring and learning management — iSpring.
+			</p>
+			<div class="enthusiasts__logos logos">
+				<div class="logos__up">
+ <img alt="Amazon" src="/local/templates/empty/images/icons/amazon.svg"> <img alt="Visa" src="/local/templates/empty/images/icons/visa.svg"> <img alt="Dell" src="/local/templates/empty/images/icons/dell.svg"> <img alt="Google" src="/local/templates/empty/images/icons/google.svg">
+				</div>
+				<div class="logos__down">
+ <img alt="Microsoft" src="/local/templates/empty/images/icons/microsoft.svg"> <img alt="Nike" src="/local/templates/empty/images/icons/nike.svg"> <img alt="Starbucks" src="/local/templates/empty/images/icons/starbucks.svg"> <img alt="Booking" src="/local/templates/empty/images/icons/booking.svg">
+				</div>
+			</div>
+		</div>
+		<div class="enthusiasts__information">
+			<div class="enthusiasts__clients">
+				<p class="enthusiasts__clients-title title title_color_white">
+					 198 clients
+				</p>
+				<p class="enthusiasts__clients-text text text_color_white">
+					 from Fortune 500 companies
+				</p>
+			</div>
+			<div class="enthusiasts__years">
+				<p class="enthusiasts__years-title title title_color_white">
+					 20 years
+				</p>
+				<p class="enthusiasts__years-text text text_color_white">
+					 on the market
+				</p>
+			</div>
+			<div class="enthusiasts__customers">
+				<p class="enthusiasts__customers-title title title_color_white">
+					 59,000 customers
+				</p>
+				<p class="enthusiasts__customers-text text text_color_white">
+					 from 172 countries
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="content__benefits benefits">
+		<p class="benefits__title title">
+			 Benefits of partnering with iSpring
+		</p>
+		<div class="benefits__block">
+			<div class="benefits__card benefits__card-one">
+ <img alt="bonuses" src="/local/templates/empty/images/icons/bonuses.svg" class="benefits__icon">
+				<p class="benefits__card-title card-title">
+					 Recurring revenue and bonuses
+				</p>
+				<p class="benefits__text card-text">
+					 Get profit from initial sales, as well as recurring revenue from subscription upgrades and license extensions and renewals.
+				</p>
+			</div>
+			<div class="benefits__card benefits__card-two">
+ <img alt="support" src="/local/templates/empty/images/icons/support.svg" class="benefits__icon">
+				<p class="benefits__card-title card-title">
+					 24/7 customer support
+				</p>
+				<p class="benefits__text card-text">
+					 We take care of post-sale customer support so you can focus on your business. Our support engineers are always ready to help.
+				</p>
+			</div>
+			<div class="benefits__card benefits__card-three">
+ <img alt="opportunities" src="/local/templates/empty/images/icons/opportunities.svg" class="benefits__icon">
+				<p class="benefits__card-title card-title">
+					 Expanded product offering
+				</p>
+				<p class="benefits__text card-text">
+					 iSpring’s software can open a door to new business opportunities, enabling you to cover a wider range of client needs.
+				</p>
+			</div>
+		</div>
+ <a href="#form" class="benefits__button button button_color_blue">Become a Partner</a>
+	</div>
+	<div class="content__jump-start jump-start">
+		<div class="jump-start__text-block">
+			<p class="jump-start__title title title_color_white">
+				 Jump-start your business growth with iSpring
+			</p>
+			<p class="jump-start__text text text_color_white">
+				 Whether you are an eLearning consultant, VAR, software reseller, distributor, or tenderer, you can take advantage of the iSpring Partner Program and start increasing your revenue today.
+			</p>
+ <a href="#form" class="jump-start__button button">Become a Partner</a>
+		</div>
+		<div class="jump-start__steps-block">
+			<div class="jump-start__steps steps">
+				<div class="circle__block">
+					<p class="steps__number steps__number_background_white">
+						 01
+					</p>
+					<p class="circle">
+					</p>
+					<p class="circle">
+					</p>
+					<p class="circle">
+					</p>
+					<p class="circle">
+					</p>
+				</div>
+				<div class="steps__text-block">
+					<p class="steps__title card-title card-title_color_white">
+						 Sign up as a partner
+					</p>
+					<p class="steps__text text text text_color_white">
+						 Fill in the <a href="#form" class="steps__link">Partner Program form</a> to join our partner network. We will contact you shortly with all the details and reseller account information.
+					</p>
+				</div>
+			</div>
+			<div class="jump-start__steps steps">
+				<div class="circle__block">
+					<p class="steps__number steps__number_color_white steps__number_background_orange">
+						 02
+					</p>
+					<p class="circle">
+					</p>
+					<p class="circle">
+					</p>
+					<p class="circle">
+					</p>
+					<p class="circle">
+					</p>
+				</div>
+				<div class="steps__text-block">
+					<p class="steps__title card-title card-title_color_white">
+						 Get welcome training
+					</p>
+					<p class="steps__text text text text_color_white">
+						 In just 2 webinars, you and your employees will master the key features of iSpring software and learn how to use your reseller account in the most effective way.
+					</p>
+				</div>
+			</div>
+			<div class="jump-start__steps steps">
+				<p class="steps__number steps__number_color_white steps__number_background_blue">
+					 03
+				</p>
+				<div class="steps__text-block">
+					<p class="steps__title card-title card-title_color_white">
+						 Close your first deal
+					</p>
+					<p class="steps__text text text text_color_white">
+						 Selling iSpring software is easy because it is both powerful and user-friendly.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="content__software software">
+		<div class="software__block">
+			<div class="software__card software__card-one software__card_color_blue software__card_vision">
+				<p class="software__card-title title title_color_white">
+					 Promote iSpring software easily
+				</p>
+			</div>
+			<div class="software__card software__card-two">
+ <img alt="licenses" src="/local/templates/empty/images/icons/licenses.svg" class="software__icon">
+				<p class="software__card-title card-title">
+					 Free demo licenses
+				</p>
+				<p class="software__text card-text">
+					 You will get free NFR licenses for all iSpring desktop products and a free iSpring Learn LMS account. These resources will be a useful aid in negotiations with your prospective clients.
+				</p>
+			</div>
+			<div class="software__card software__card-three">
+ <img alt="Marketing aids" src="/local/templates/empty/images/icons/aids.svg" class="software__icon">
+				<p class="software__card-title card-title">
+					 Marketing aids
+				</p>
+				<p class="software__text card-text">
+					 We support our partners at all stages of the sales process: from lead generation to closing and follow-up. We also provide ready-made marketing aids: guidelines, presentations, sales proposals, etc.
+				</p>
+			</div>
+			<div class="software__card software__card-four">
+ <img alt="Co-marketing activities" src="/local/templates/empty/images/icons/activities.svg" class="software__icon">
+				<p class="software__card-title card-title">
+					 Co-marketing activities
+				</p>
+				<p class="software__text card-text">
+					 We are always here to help you grow the market and expand your customer base. Our marketing specialists gladly take part in partners’ promotional activities, like workshops and conferences.
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="content__manager manager">
+ <img alt="manage sales" src="/local/templates/empty/images/manage_sales.png">
+		<div class="manadger__text-block">
+			<p class="manadger__title title">
+				 Manage all your sales online
+			</p>
+			<p class="manadger__text card-text">
+				 All partners get access to iSpring’s reseller portal. There you can process orders and license delivery, download license certificates and receipts, and view sales history with ease.
+			</p>
+ <a href="#form" class="manadger__button button button_color_blue">Become a Partner</a>
+		</div>
+	</div>
+	<div class="content__language language">
+		<p class="language__title title">
+			 Products available in 17 languages
+		</p>
+		<p class="language__text card-text">
+			 iSpring software and services support a variety of languages to provide exceptional user experience to users from different countries. New languages are being covered on a regular basis.
+		</p>
+		<div class="language__block">
+			<div class="language__single-unit single-unit">
+				<p class="single-unit__circle">
+				</p>
+				<p class="single-unit__language">
+					 English
+				</p>
+			</div>
+			<div class="language__single-unit single-unit">
+				<p class="single-unit__circle">
+				</p>
+				<p class="single-unit__language">
+					 Spanish
+				</p>
+			</div>
+			<div class="language__single-unit single-unit">
+				<p class="single-unit__circle">
+				</p>
+				<p class="single-unit__language">
+					 French
+				</p>
+			</div>
+			<div class="language__single-unit single-unit">
+				<p class="single-unit__circle">
+				</p>
+				<p class="single-unit__language">
+					 German
+				</p>
+			</div>
+			<div class="language__single-unit single-unit">
+				<p class="single-unit__circle">
+				</p>
+				<p class="single-unit__language">
+					 Italian
+				</p>
+			</div>
+			<div class="language__single-unit single-unit">
+				<p class="single-unit__circle">
+				</p>
+				<p class="single-unit__language">
+					 Japanese
+				</p>
+			</div>
+			<div class="language__single-unit single-unit">
+				<p class="single-unit__circle">
+				</p>
+				<p class="single-unit__language">
+					 Portuguese
+				</p>
+			</div>
+			<div class="language__single-unit single-unit">
+				<p class="single-unit__circle">
+				</p>
+				<p class="single-unit__language">
+					 Russian
+				</p>
+			</div>
+			<div class="language__single-unit single-unit">
+				<p class="single-unit__circle">
+				</p>
+				<p class="single-unit__language">
+					 Polish
+				</p>
+			</div>
+			<div class="language__single-unit single-unit">
+				<p class="single-unit__circle">
+				</p>
+				<p class="single-unit__language">
+					 Uzbek
+				</p>
+			</div>
+			<div class="language__single-unit single-unit">
+				<p class="single-unit__circle">
+				</p>
+				<p class="single-unit__language">
+					 Chinese Simplified
+				</p>
+			</div>
+			<div class="language__single-unit single-unit">
+				<p class="single-unit__circle">
+				</p>
+				<p class="single-unit__language">
+					 Chinese Traditional
+				</p>
+			</div>
+			<div class="language__single-unit single-unit">
+				<p class="single-unit__circle">
+				</p>
+				<p class="single-unit__language">
+					 Mongolian
+				</p>
+			</div>
+			<div class="language__single-unit single-unit">
+				<p class="single-unit__circle">
+				</p>
+				<p class="single-unit__language">
+					 Norwegian
+				</p>
+			</div>
+			<div class="language__single-unit single-unit">
+				<p class="single-unit__circle">
+				</p>
+				<p class="single-unit__language">
+					 Ukrainian
+				</p>
+			</div>
+			<div class="language__single-unit single-unit">
+				<p class="single-unit__circle">
+				</p>
+				<p class="single-unit__language">
+					 Indonesian
+				</p>
+			</div>
+			<div class="language__single-unit single-unit">
+				<p class="single-unit__circle">
+				</p>
+				<p class="single-unit__language">
+					 Finnish
+				</p>
+			</div>
+			<div class="language__single-unit single-unit">
+				<p class="single-unit__circle">
+				</p>
+				<p class="single-unit__language">
+					 Azerbaijani
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="content__questions questions" id="questions">
+		<div class="questions__block-title">
+ <img alt="hands" src="/local/templates/empty/images/icons/hands.svg">
+			<p class="questions__title title title_color_white">
+				 Have questions? Reach out!
+			</p>
+		</div>
+		<div class="questions__block-text">
+			<p class="questions__text">
+				 We provide our partners with fast tech and sales assistance, including pre-sales and sales support.
+			</p>
+			<p class="questions__text questions__text_margin">
+				 Contact us any time at <a href="mailto:partner@ispring.com">partner@ispring.com</a>.
+			</p>
+		</div>
+	</div>
+	
+			<?$APPLICATION->IncludeComponent(
+	"bitrix:news.list", 
+	"index", 
+	array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "N",
+		"DISPLAY_DATE" => "N",
+		"DISPLAY_NAME" => "N",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => array(
+			0 => "NAME",
+			1 => "PREVIEW_PICTURE",
+			2 => "",
+		),
+		"FILTER_NAME" => "",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => "1",
+		"IBLOCK_TYPE" => "index",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+		"INCLUDE_SUBSECTIONS" => "N",
+		"MESSAGE_404" => "",
+		"NEWS_COUNT" => "20",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Новости",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PROPERTY_CODE" => array(
+			0 => "fio",
+			1 => "post",
+			2 => "review",
+			3 => "",
+		),
+		"SET_BROWSER_TITLE" => "N",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "N",
+		"SET_META_KEYWORDS" => "N",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "N",
+		"SHOW_404" => "N",
+		"SORT_BY1" => "SORT",
+		"SORT_BY2" => "TIMESTAMP_X",
+		"SORT_ORDER1" => "ASC",
+		"SORT_ORDER2" => "DESC",
+		"STRICT_SECTION_CHECK" => "N",
+		"COMPONENT_TEMPLATE" => "index"
+	),
+	false
+);?>
+<div class="content__form">
+	<div class="content__form-size">
+		<div class="content__form-block">
+			<p class="content__form-vector">
+			</p>
+			<p class="content__form-title title title_color_white">
+				 Become an iSpring Partner
+			</p>
+			<p class="content__form-text">
+				 Tell us about yourself and your company.
+			</p>
+		</div>
+		<form class="content__form-unit form" id="form">
+			<p class="form__title">
+				 Company Information
+			</p>
+ <input type="text" class="form__place name" placeholder="Company name" required=""> <input type="tel" class="form__place phone" name="phone" pattern="+ [7]-[0-9]{3}-[0-9]{3}-[0-9]{4}" required="" placeholder="Phone"> <input type="email" name="Email" placeholder="Email" class="form__place email"> <textarea name="message" placeholder="Brief company description (years in business, eLearning experience, and why you want to be an iSpring partner) " class="form__place textarea"></textarea>
+			<p class="form__text">
+				 By clicking "Become a Partner," you confirm that you're agreeing to our <a href="#" class="form__text form__text-link">Terms</a> and <a href="#" class="form__text form__text-link">Privacy Policy</a>.
+			</p>
+ <input type="submit" name="submit" value="Become a Partner" class="form__button">
+		</form>
+	</div>
+</div>
+ <br><?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+?>
